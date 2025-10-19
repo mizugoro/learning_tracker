5e9a000bb35748f5de6d2c420ee93e655d741ffd
mizugoro <takenoko12310121@gmail.com>
2025-10-19 17:08:31 +0900

    add github actions settings

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@
   import path from 'path';
 
   export default defineConfig({
+    base: '/learning-tracker/',
     plugins: [react()],
     resolve: {
       extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
